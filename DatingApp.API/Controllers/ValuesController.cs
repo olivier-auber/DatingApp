4bcbb78678bf8aad36d78892023f640ba0abc1f6
Olivier Auber <olivier.auber@gmail.com>
2018-12-15 14:59:45 -0500

    Creating the first versiongit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = context.Values.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = context.Values.FirstOrDefault(v => v.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
